bc3d83b9ad37f7da5b5e386ff4a55c83353b5e0f
Juan Sebastian Cadena Varela <juan.cadena02@uceva.edu.co>
2026-03-22 21:37:39 -0500

    Inicio de estructura de FastAPI, actualización de .gitignore, requerimientos, configuración inicial de BD y modelo inicial de BD

---
+++ config/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DB_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except Exception:
+        db.rollback()
+        raise
+    finally:
+        db.close()
